5e51675f555bdd4b48b61281e47ca331ad1c084d
Zao0oza <sefeigin@mail.ru>
2021-05-23 21:41:56 +0300

    init

--- bushido.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from os import getcwd, path, makedirs
-
-with open('C:\mainbot\settings.yaml', encoding='utf-8') as f:
-    templates = yaml.safe_load(f)
-
-    print(templates['dictionary'])
-
