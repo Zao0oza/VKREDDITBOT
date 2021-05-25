0ef00f7bdf06a15fd9a0de421ce16308b995cb5a
Zao0oza <sefeigin@mail.ru>
2021-05-25 16:29:39 +0300

    test

--- main.py
+++ main.py
@@ -17,30 +17,11 @@ if not path.exists(filename + '/data/'):
 
 with open(filename + '/data/log.txt', "a") as f:
     f.write(str(datetime.datetime.now()) + '\n')
-
-while True:
-    r = sr.Recognizer()
-    bot_session = vk_api.VkApi(
-        token=tokens['tokens'])
-    bot_api = bot_session.get_api()
-    upload = VkUpload(bot_session)
-    longpoll = VkBotLongPoll(bot_session, tokens['group_id'])
-    keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button(':tits', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button(':бушидо', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button(':сиси', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button(':Hello there', color=VkKeyboardColor.PRIMARY)
-
-    try:
-        def main():
-            try:
-                for event in longpoll.listen():  # регистрируем событие
-                    print("got event")
-                    print(event.obj.peer_id)
-                    search_list = event.object['text'].lower().split(':')
-
-                    try:
-                        if event.object['attachments'][0]['type'] == 'audio_message':
+    
+def new_event(event):
+	search_list = event.object['text'].lower().split(':')
+	try:
+	                           if event.object['attachments'][0]['type'] == 'audio_message':
                             wavv = requests.get(event.object['attachments'][0]['audio_message']['link_mp3']).content
                             sound = AudioSegment.from_mp3(BytesIO(wavv))
                             sound.export(filename + '/sound.wav', format="wav")
@@ -79,6 +60,27 @@ while True:
                         send_photo(bot_api, event.obj.peer_id,
                                    *upload_photo(upload, search_reddit(search_list[1] + ' nsfw:1'), True,''))
                         tier(event.obj.peer_id,event.object.from_id, search_list[-1])
+while True:
+    r = sr.Recognizer()
+    bot_session = vk_api.VkApi(
+        token=tokens['tokens'])
+    bot_api = bot_session.get_api()
+    upload = VkUpload(bot_session)
+    longpoll = VkBotLongPoll(bot_session, tokens['group_id'])
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button(':tits', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button(':бушидо', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button(':сиси', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button(':Hello there', color=VkKeyboardColor.PRIMARY)
+
+    try:
+        def main():
+            try:
+                for event in longpoll.listen():  # регистрируем событие
+                    print("got event")
+                    print(event.obj.peer_id
+                    event_new(event)
+
 
             except Exception as err:
                 print(err)
